4e24cbeb515e8045f98d557386a7049636c65fd5
Anastasia <shadowrun777@ukr.net>
2018-10-29 23:31:53 +0200

    blank=True, null=True2

--- blog/views.py
+++ blog/views.py
@@ -84,6 +84,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.title(blank=True, null=True)
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -133,6 +134,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.title(blank=True, null=True)
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
