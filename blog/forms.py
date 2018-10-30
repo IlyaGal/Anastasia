3dea1a7bf54108aedbe5712661e9e75c4011167c
Anastasia <shadowrun777@ukr.net>
2018-10-30 21:50:42 +0200

    link

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text', 'hierarchy')
+        fields = ('title', 'text', 'hierarchy', 'link')
 
 class AspirantForm(forms.ModelForm):
 
--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    link = models.URLField(max_length=200, blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
