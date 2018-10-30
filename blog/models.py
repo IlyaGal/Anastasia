47813d3fff90b817f43cfa228a2e5a303351cc2a
Anastasia <shadowrun777@ukr.net>
2018-10-30 13:43:17 +0200

    unique=True + blank=True, null=True

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=200, blank=True, null=True)
-    text = models.TextField()
-    hierarchy = models.IntegerField(default=1)
+    text = models.TextField(blank=True, null=True)
+    hierarchy = models.IntegerField(unique=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -20,13 +20,13 @@ class Post(models.Model):
         return self.title
 
 class Test(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
 
-    title = models.CharField(max_length=200)
-    kurator = models.TextField(default=1)
-    diser_name = models.TextField(default=1)
-    text = models.TextField()
-    hierarchy = models.IntegerField(default=1)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    kurator = models.TextField(blank=True, null=True)
+    diser_name = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    hierarchy = models.IntegerField(unique=True)
 
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -41,13 +41,13 @@ class Test(models.Model):
         return self.title
 
 class Intresting(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
 
-    title = models.CharField(max_length=200)
-    kurator = models.TextField(default=1)
-    diser_name = models.TextField(default=1)
-    text = models.TextField()
-    hierarchy = models.IntegerField(default=1)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    kurator = models.TextField(blank=True, null=True)
+    diser_name = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    hierarchy = models.IntegerField(unique=True)
 
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -62,18 +62,18 @@ class Intresting(models.Model):
         return self.title
 
 class Aspirant(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    kurator = models.TextField(default=1)
-    diser_name = models.TextField(default=1)
-    text = models.TextField()
-    hierarchy = models.IntegerField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    kurator = models.TextField(blank=True, null=True)
+    diser_name = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    hierarchy = models.IntegerField(unique=True)
     tests = models.ManyToManyField(
         to=Test,
-        related_name='aspirants')
+        related_name='aspirants', blank=True, null=True)
     intrestings = models.ManyToManyField(
         to=Intresting,
-        related_name='aspirants')
+        related_name='aspirants', blank=True, null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
