7effb04e8c4e1e498998744ebaa8de0f62a5707f
Anastasia <shadowrun777@ukr.net>
2019-02-13 03:01:41 +0200

    table3

--- blog/urls.py
+++ blog/urls.py
@@ -26,4 +26,9 @@ urlpatterns = [
     url(r'^bookAnastasia/$', views.book_anastasia),
     url(r'^Aspirants/$', views.stat_aspirants),
     url(r'^AnastasiaKolorova/$', views.stat_anastasia),
+    url(r'^page1/$', views.stat_page1),
+    url(r'^page2/$', views.stat_page2),
+    url(r'^page3/$', views.stat_page3),
+    url(r'^page4/$', views.stat_page4),
+    url(r'^page5/$', views.stat_page5),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,16 @@ from django.utils import timezone
 from .models import Post, Aspirant, Test, Intresting
 from .forms import PostForm, AspirantForm, TestForm, IntrestingForm
 
+def stat_page1(request):
+    return render(request, 'blog/page1.html')
+def stat_page2(request):
+    return render(request, 'blog/page2.html')
+def stat_page3(request):
+    return render(request, 'blog/page3.html')
+def stat_page4(request):
+    return render(request, 'blog/page4.html')
+def stat_page5(request):
+    return render(request, 'blog/page5.html')
 def stat_aspirants(request):
     return render(request, 'blog/Aspirants.html')
 def stat_anastasia(request):
